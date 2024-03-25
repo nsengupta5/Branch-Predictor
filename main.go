1573dc85bf2455cdc940b2555b0a6a252567a774
Nikhil Sengupta <nikhil.sengupta10@protonmail.com>
2024-03-25 16:20:48 +0000

    Starting point

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+// This file contains the main function to run the cache simulator.
+// It reads in the command line arguements and initializes the cache configuration
+// and the cache simulator.
+
+import (
+	"os"
+
+	"github.com/nsengupta5/Cache-Simulator/cache"
+	"github.com/nsengupta5/Cache-Simulator/instruction"
+)
+
+func main() {
+	// Read in the command line arguements
+	configFile := os.Args[1]
+	traceFile := os.Args[2]
+
+	// Initialize the cache configuration and the cache simulator
+	config := cache.InitializeConfig(configFile)
+	cache.InitializeCaches(&config)
+	simulator := instruction.NewCacheSimulator(&config)
+
+	// Execute the cache simulator
+	simulator.Execute(traceFile)
+}
